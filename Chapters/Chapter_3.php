c70ec1575c81fd12cac23d71078abe5f981fcabc
R JEEVAN KUMAR <g1kumar215@gmail.com>
2024-12-16 18:26:15 +0530

    My First PHP Program

---
+++ Chapters/Chapter_3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP FIRST Program</title>
+</head>
+<body>
+    <center>
+    <marquee>This is my First PHP Program</marquee>
+    <?php
+        echo "Hello World! to php in VSCODE";
+        echo "<br/><font color='red' size='5'>Hello World! to php in VSCODE</font>";
+        echo "<br/>This is my PHP certification Course";
+        print("Hello World! to php in VSCODE using print function");
+        echo "Arthematics Operation using php";
+        $a=10;
+        $b=20;
+        $c=$a+$b;
+        echo "<br/>$a+$b=$c";
+        echo "<br/>$a-$b=".($a-$b);
+        echo "<br/>$a*$b=".($a*$b);
+        echo "<br/>$a/$b=".($a/$b);
+        echo "<br/>$a%$b=".($a%$b);
+        echo "<br/>$a**$b=".($a**$b);
+
+    ?>
+    <center>
+</body>
+</html>
